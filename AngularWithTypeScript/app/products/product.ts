3df33ffd47565e00b22449690ab61e25f7b44573
Filip Mitkovski <filip.mitkovski@aerdata.com>
2016-01-13 17:07:22 +0100

    added interface for the product entity

---
+++ AngularWithTypeScript/app/products/product.ts
@@ -0,0 +1,13 @@
+﻿module app.domain {
+    interface IProduct {
+        productId: number;
+        productName: string;
+        productCode: string;
+        releaseDate: Date;
+        price: number;
+        description: string;
+        imageUrl: string;
+        calculateDiscount(percent: number): number;
+    }
+
+}
